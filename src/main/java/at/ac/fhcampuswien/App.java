aaeb3b9c015c4a4f0482b9ad9ce4e49f7bcabefe
Collins Osama <collins.osama@stud.fh-campuswien.ac.at>
2022-10-01 19:37:14 +0200

    Task 1 ( problem mit der Nachkommastelle double)

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,9 +5,39 @@ import java.util.Scanner;
 public class App {
 
     //todo Task 1
-    public void largestNumber(){
-        // input your solution here
+    public void largestNumber() {
+
+        new java.text.DecimalFormat("0.00").format(1.742545452562);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Number 1: ");         double num1 = scan.nextInt();
+
+        if (num1 <= 0){
+
+            System.out.println("No number entered.");
+
+            return;
+        }
+
+        System.out.print("Number 2: ");         double num2 = scan.nextInt();
+
+        System.out.print("Number 3: ");         double num3 = scan.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+
+            System.out.println("The largest number is " + num1);
+
+        } else if (num2 > num1 && num2 > num3) {
+
+            System.out.println("The largest number is " + num2);
+
+        }else System.out.print("The largest number is " + num3);
+
+
+
     }
+
 
     //todo Task 2
     public void stairs(){
